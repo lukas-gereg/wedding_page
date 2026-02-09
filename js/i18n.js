bf0d1327ef62a4b768469f19f971bd576ac50207
Lukas <luke.gereg@gmail.com>
2026-02-09 20:41:42 +0100

    form fields modifications

--- js/i18n.js
+++ js/i18n.js
@@ -1,248 +1,257 @@
 const I18N = {
-  sk: {
-    nav_home: "Domov",
-    nav_rsvp: "RSVP",
-    nav_story: "Náš príbeh",
-    nav_gallery: "Galéria",
-    nav_wherewhen: "Kde & Kedy",
-    lang_label: "Jazyk",
+    sk: {
+        nav_home: "Domov",
+        nav_rsvp: "RSVP",
+        nav_story: "Náš príbeh",
+        nav_gallery: "Galéria",
+        nav_wherewhen: "Kde & Kedy",
+        lang_label: "Jazyk",
 
-    home_kicker: "Pozývame Vás na svadbu",
-    home_title: "Anna & Lukáš",
-    home_sub: "Sobota • 15. august 2026 • 14:00 • Budapešť, Fő utca 17.",
-    home_btn_rsvp: "Potvrdiť účasť (RSVP)",
-    home_btn_details: "Kde & Kedy",
+        home_kicker: "Pozývame Vás na svadbu",
+        home_title: "Anna & Lukáš",
+        home_sub: "Sobota • 15. august 2026 • 14:00 • Budapešť, Fő utca 17.",
+        home_btn_rsvp: "Potvrdiť účasť (RSVP)",
+        home_btn_details: "Kde & Kedy",
 
-    rsvp_title: "RSVP",
-    rsvp_sub: "Prosíme, dajte nám vedieť do: (doplňte dátum)",
-    rsvp_note: "Tu vložte váš Google Form alebo vlastný formulár.",
+        rsvp_title: "RSVP",
+        rsvp_sub: "Prosíme, dajte nám vedieť do: (doplňte dátum)",
+        rsvp_note: "Tu vložte váš Google Form alebo vlastný formulár.",
 
-    story_title: "Náš príbeh",
-    story_p1: "Sem príde váš príbeh. Krátke odseky vyzerajú na tomto štýle najkrajšie.",
-    story_p2: "Môžete doplniť aj časovú os, fotky, alebo malé citáty.",
+        story_title: "Náš príbeh",
+        story_p1: "Sem príde váš príbeh. Krátke odseky vyzerajú na tomto štýle najkrajšie.",
+        story_p2: "Môžete doplniť aj časovú os, fotky, alebo malé citáty.",
 
-    gallery_title: "Galéria",
-    gallery_sub: "Fotky pridáme čoskoro.",
+        gallery_title: "Galéria",
+        gallery_sub: "Fotky pridáme čoskoro.",
 
-    ww_title: "Kde & Kedy",
-    ww_ceremony: "Obrad",
-    ww_reception: "Hostina",
-    ww_date: "Dátum",
-    ww_time: "Čas",
-    ww_place: "Miesto",
-    ww_map: "Mapa",
+        ww_title: "Kde & Kedy",
+        ww_ceremony: "Obrad",
+        ww_reception: "Hostina",
+        ww_date: "Dátum",
+        ww_time: "Čas",
+        ww_place: "Miesto",
+        ww_map: "Mapa",
 
-    choose: "Vyber…",
+        choose: "Vyber…",
 
-  q1_title: "Účasť",
-  q1_label: "Zúčastním sa:",
-  q1_a1: "Obrad & omša (agapé)",
-  q1_a2: "Obrad & hostina (agapé)",
-  q1_a3: "Celý deň",
-  q1_a4: "Nemôžem prísť",
+        q1_title: "Účasť",
+        q1_label: "Zúčastním sa:",
+        q1_a1: "Obrad & omša (agapé)",
+        q1_a2: "Obrad & hostina (agapé)",
+        q1_a3: "Celý deň",
+        q1_a4: "Nemôžem prísť",
 
-  q2_title: "Môžem priniesť",
-  q2_cookies: "Koláčiky",
-  q2_salty: "Slané",
-  q2_sweet: "Sladké",
-  q2_dietary: "Diétne / špeciálne",
-  q2_nothing: "Nemôžem nič priniesť",
-  q2_other: "Iné",
-  q2_none: "Nič",
+        q2_title: "Môžem priniesť",
+        q2_cookies: "Koláčiky",
+        q2_salty: "Slané",
+        q2_sweet: "Sladké",
+        q2_dietary: "Diétne / špeciálne",
+        q2_nothing: "Nemôžem nič priniesť",
+        q2_other: "Iné",
+        q2_none: "Nič",
 
-  q3_title: "Môžem pomôcť s",
-  q3_hint: "Vyberte, čo sa hodí:",
-  q3_1: "Výzdoba deň vopred",
-  q3_2: "Výzdoba v kostole",
-  q3_3: "Upratovanie/balenie po agapé",
-  q3_4: "Tlač / rozdávanie liturgických textov a piesní",
-  q3_5: "Detský kútik (na zmeny)",
-  q3_6: "Prskavky / zbieranie vecí",
-  q3_7: "Nalievanie welcome drinku",
-  q3_8: "Pozývanie ľudí na parket",
-  q3_9: "Odvoz / shuttle",
-  q3_10: "Balenie ráno po svadbe",
-  q3_other: "Iné",
+        q3_title: "Môžem pomôcť s",
+        q3_hint: "Vyberte, čo sa hodí:",
+        q3_1: "Výzdoba deň vopred",
+        q3_2: "Výzdoba v kostole",
+        q3_3: "Upratovanie/balenie po agapé",
+        q3_4: "Tlač / rozdávanie liturgických textov a piesní",
+        q3_5: "Detský kútik (na zmeny)",
+        q3_6: "Prskavky / zbieranie vecí",
+        q3_7: "Nalievanie welcome drinku",
+        q3_8: "Pozývanie ľudí na parket",
+        q3_9: "Odvoz / shuttle",
+        q3_10: "Balenie ráno po svadbe",
+        q3_other: "Iné",
 
-  q4_title: "Príchod & ubytovanie",
-  q4_1: "Piatok večer (2 noci ubytovanie)",
-  q4_2: "Sobota ráno (1 noc ubytovanie)",
-  q4_3: "Ubytovanie nepotrebujem",
+        q4_title: "Príchod & ubytovanie",
+        q4_1: "Piatok večer (2 noci ubytovanie)",
+        q4_2: "Sobota ráno (1 noc ubytovanie)",
+        q4_3: "Ubytovanie nepotrebujem",
 
-  q5_title: "Auto / spolujazda",
-  q5_spaces: "Voľné miesta v aute",
-  q5_route: "Trasa / smer",
+        q5_title: "Auto / spolujazda",
+        q5_spaces: "Voľné miesta v aute",
+        q5_route: "Trasa / smer",
 
-  q6_title: "Stravovanie",
-  q6_1: "Bez obmedzení",
-  q6_2: "Vegánske",
-  q6_3: "Bezlepkové",
-  q6_4: "Bez laktózy",
-  q6_5: "Bez mlieka (dairy-free)",
-  q6_other: "Iné",
+        q6_title: "Stravovanie",
+        q6_1: "Bez obmedzení",
+        q6_2: "Vegánske",
+        q6_3: "Bezlepkové",
+        q6_4: "Bez laktózy",
+        q6_5: "Bez mlieka (dairy-free)",
+        q6_other: "Iné",
 
-  q7_title: "Zodpovednosť za alergie",
-  q7_text: "Rozumiem, že v prípade alergickej reakcie nesiem plnú zodpovednosť za svoje poistenie a lieky.",
+        q7_title: "Zodpovednosť za alergie",
+        q7_text: "Rozumiem, že v prípade alergickej reakcie nesiem plnú zodpovednosť za svoje poistenie a lieky.",
 
-  q8_title: "Dar",
-  q8_1: "Finančný príspevok",
-  q8_2: "Malý darček / prekvapenie",
-  q8_3: "Darček + peniaze",
-  q8_4: "Prispejem na náš domáci wishlist",
-  q8_wishlist_link: "Wishlist link (Excel)",
+        q8_title: "Dar",
+        q8_1: "Finančný príspevok",
+        q8_2: "Malý darček / prekvapenie",
+        q8_3: "Darček + peniaze",
+        q8_4: "Prispejem na náš domáci wishlist",
+        q8_wishlist_link: "Wishlist link (Excel)",
 
-  q9_title: "Kontakt",
-  q9_phone: "Telefón",
-  q10_email: "Email",
-  q11_name: "Celé úradné meno",
+        q9_title: "Kontakt",
+        q9_phone: "Telefón",
+        q10_email: "Email",
+        q11_name: "Celé úradné meno",
 
-  consent_title: "Súhlasy",
-  consent_feedback: "Súhlasím, že poskytnem finálnu spätnú väzbu ~30 dní pred svadbou cez emailové pripomenutie.",
-  consent_media: "Súhlasím, aby som ja (a moje deti) mohli byť na fotkách/videách z udalosti (GDPR).",
+        consent_title: "Súhlasy",
+        consent_feedback: "Súhlasím, že poskytnem finálnu spätnú väzbu ~30 dní pred svadbou cez emailové pripomenutie.",
+        consent_media: "Súhlasím, aby som ja (a moje deti) mohli byť na fotkách/videách z udalosti (GDPR).",
+        other_placeholder: "Iné…",
+        amount_placeholder: "Suma",
 
-  submit: "Odoslať"
-  },
+        // Optional: if you want a label above help_other input
+        q3_other_label: "Iné (dopíšte):",
 
-  hu: {
-    nav_home: "Kezdőlap",
-    nav_rsvp: "RSVP",
-    nav_story: "Történetünk",
-    nav_gallery: "Galéria",
-    nav_wherewhen: "Hol & Mikor",
-    lang_label: "Nyelv",
+        submit: "Odoslať"
+    },
 
-    home_kicker: "Szeretettel meghívunk az esküvőnkre",
-    home_title: "Anna & Lukáš",
-    home_sub: "Szombat • 2026. augusztus 15. • 14:00 • Budapest, Fő utca 17.",
-    home_btn_rsvp: "Részvétel visszajelzés (RSVP)",
-    home_btn_details: "Hol & Mikor",
+    hu: {
+        nav_home: "Kezdőlap",
+        nav_rsvp: "RSVP",
+        nav_story: "Történetünk",
+        nav_gallery: "Galéria",
+        nav_wherewhen: "Hol & Mikor",
+        lang_label: "Nyelv",
 
-    rsvp_title: "RSVP",
-    rsvp_sub: "Kérjük jelezzetek vissza eddig: (írjátok be a dátumot)",
-    rsvp_note: "Ide illesszétek be a Google Űrlapot vagy saját űrlapot.",
+        home_kicker: "Szeretettel meghívunk az esküvőnkre",
+        home_title: "Anna & Lukáš",
+        home_sub: "Szombat • 2026. augusztus 15. • 14:00 • Budapest, Fő utca 17.",
+        home_btn_rsvp: "Részvétel visszajelzés (RSVP)",
+        home_btn_details: "Hol & Mikor",
 
-    story_title: "Történetünk",
-    story_p1: "Ide jön a történetetek. Rövid bekezdések ehhez a stílushoz passzolnak a legjobban.",
-    story_p2: "Később lehet idővonal, képek vagy idézetek is.",
+        rsvp_title: "RSVP",
+        rsvp_sub: "Kérjük jelezzetek vissza eddig: (írjátok be a dátumot)",
+        rsvp_note: "Ide illesszétek be a Google Űrlapot vagy saját űrlapot.",
 
-    gallery_title: "Galéria",
-    gallery_sub: "Hamarosan jövünk a fotókkal.",
+        story_title: "Történetünk",
+        story_p1: "Ide jön a történetetek. Rövid bekezdések ehhez a stílushoz passzolnak a legjobban.",
+        story_p2: "Később lehet idővonal, képek vagy idézetek is.",
 
-    ww_title: "Hol & Mikor",
-    ww_ceremony: "Szertartás",
-    ww_reception: "Lakodalom",
-    ww_date: "Dátum",
-    ww_time: "Időpont",
-    ww_place: "Helyszín",
-    ww_map: "Térkép",
+        gallery_title: "Galéria",
+        gallery_sub: "Hamarosan jövünk a fotókkal.",
 
-    choose: "Válassz…",
+        ww_title: "Hol & Mikor",
+        ww_ceremony: "Szertartás",
+        ww_reception: "Lakodalom",
+        ww_date: "Dátum",
+        ww_time: "Időpont",
+        ww_place: "Helyszín",
+        ww_map: "Térkép",
 
-  q1_title: "Részvétel",
-  q1_label: "Részt veszek:",
-  q1_a1: "Szertartás & mise (agapé)",
-  q1_a2: "Szertartás & vacsora (agapé)",
-  q1_a3: "Egész nap",
-  q1_a4: "Nem tudok jönni",
+        choose: "Válassz…",
 
-  q2_title: "Amit tudok hozni",
-  q2_cookies: "Sütemény",
-  q2_salty: "Sós rágcsa",
-  q2_sweet: "Édesség",
-  q2_dietary: "Diétás / speciális",
-  q2_nothing: "Nem tudok hozni semmit",
-  q2_other: "Egyéb",
-  q2_none: "Semmi",
+        q1_title: "Részvétel",
+        q1_label: "Részt veszek:",
+        q1_a1: "Szertartás & mise (agapé)",
+        q1_a2: "Szertartás & vacsora (agapé)",
+        q1_a3: "Egész nap",
+        q1_a4: "Nem tudok jönni",
 
-  q3_title: "Tudok segíteni",
-  q3_hint: "Válassz ki bármit, ami oké:",
-  q3_1: "Dekorálás előző nap",
-  q3_2: "Dekorálás a templomban",
-  q3_3: "Pakolás az agapé után",
-  q3_4: "Liturgikus szövegek és énekek nyomtatása / kiosztása",
-  q3_5: "Gyereksarok (váltásban)",
-  q3_6: "Csillagszórók osztása / összegyűjtés",
-  q3_7: "Welcome drink kitöltése",
-  q3_8: "Emberek hívása a tánctérre",
-  q3_9: "Fuvar / shuttle",
-  q3_10: "Pakolás másnap reggel",
-  q3_other: "Egyéb",
+        q2_title: "Amit tudok hozni",
+        q2_cookies: "Sütemény",
+        q2_salty: "Sós rágcsa",
+        q2_sweet: "Édesség",
+        q2_dietary: "Diétás / speciális",
+        q2_nothing: "Nem tudok hozni semmit",
+        q2_other: "Egyéb",
+        q2_none: "Semmi",
 
-  q4_title: "Érkezés & szállás",
-  q4_1: "Péntek este (2 éj szállás)",
-  q4_2: "Szombat reggel (1 éj szállás)",
-  q4_3: "Nem kérek szállást",
+        q3_title: "Tudok segíteni",
+        q3_hint: "Válassz ki bármit, ami oké:",
+        q3_1: "Dekorálás előző nap",
+        q3_2: "Dekorálás a templomban",
+        q3_3: "Pakolás az agapé után",
+        q3_4: "Liturgikus szövegek és énekek nyomtatása / kiosztása",
+        q3_5: "Gyereksarok (váltásban)",
+        q3_6: "Csillagszórók osztása / összegyűjtés",
+        q3_7: "Welcome drink kitöltése",
+        q3_8: "Emberek hívása a tánctérre",
+        q3_9: "Fuvar / shuttle",
+        q3_10: "Pakolás másnap reggel",
+        q3_other: "Egyéb",
 
-  q5_title: "Autó / telekocsi",
-  q5_spaces: "Szabad helyek az autómban",
-  q5_route: "Útvonal / irány",
+        q4_title: "Érkezés & szállás",
+        q4_1: "Péntek este (2 éj szállás)",
+        q4_2: "Szombat reggel (1 éj szállás)",
+        q4_3: "Nem kérek szállást",
 
-  q6_title: "Étkezési igények",
-  q6_1: "Nincs megkötés",
-  q6_2: "Vegán",
-  q6_3: "Gluténmentes",
-  q6_4: "Laktózmentes",
-  q6_5: "Tejmentes",
-  q6_other: "Egyéb",
+        q5_title: "Autó / telekocsi",
+        q5_spaces: "Szabad helyek az autómban",
+        q5_route: "Útvonal / irány",
 
-  q7_title: "Allergia felelősség",
-  q7_text: "Tudomásul veszem, hogy allergiás reakció esetén a saját biztosításomért és gyógyszereimért teljes felelősséget vállalok.",
+        q6_title: "Étkezési igények",
+        q6_1: "Nincs megkötés",
+        q6_2: "Vegán",
+        q6_3: "Gluténmentes",
+        q6_4: "Laktózmentes",
+        q6_5: "Tejmentes",
+        q6_other: "Egyéb",
 
-  q8_title: "Ajándék",
-  q8_1: "Pénzbeli hozzájárulás",
-  q8_2: "Kis ajándék / meglepetés",
-  q8_3: "Ajándék + pénz",
-  q8_4: "Hozzájárulás az otthoni wishlisthoz",
-  q8_wishlist_link: "Wishlist link (Excel)",
+        q7_title: "Allergia felelősség",
+        q7_text: "Tudomásul veszem, hogy allergiás reakció esetén a saját biztosításomért és gyógyszereimért teljes felelősséget vállalok.",
 
-  q9_title: "Kapcsolat",
-  q9_phone: "Telefonszám",
-  q10_email: "Email",
-  q11_name: "Teljes hivatalos név",
+        q8_title: "Ajándék",
+        q8_1: "Pénzbeli hozzájárulás",
+        q8_2: "Kis ajándék / meglepetés",
+        q8_3: "Ajándék + pénz",
+        q8_4: "Hozzájárulás az otthoni wishlisthoz",
+        q8_wishlist_link: "Wishlist link (Excel)",
 
-  consent_title: "Hozzájárulások",
-  consent_feedback: "Hozzájárulok, hogy ~30 nappal az esemény előtt email emlékeztetőre végső visszajelzést adjak.",
-  consent_media: "Hozzájárulok, hogy én (és gyermekeim) szerepelhetünk az esemény fotóin/videóin (GDPR).",
+        q9_title: "Kapcsolat",
+        q9_phone: "Telefonszám",
+        q10_email: "Email",
+        q11_name: "Teljes hivatalos név",
 
-  submit: "Küldés"
-  }
+        other_placeholder: "Egyéb…",
+        amount_placeholder: "Összeg",
+        q3_other_label: "Egyéb (írd le):",
+
+        consent_title: "Hozzájárulások",
+        consent_feedback: "Hozzájárulok, hogy ~30 nappal az esemény előtt email emlékeztetőre végső visszajelzést adjak.",
+        consent_media: "Hozzájárulok, hogy én (és gyermekeim) szerepelhetünk az esemény fotóin/videóin (GDPR).",
+
+        submit: "Küldés"
+    }
 };
 
 function getLang() {
-  return localStorage.getItem("lang") || "sk";
+    return localStorage.getItem("lang") || "sk";
 }
 
 function setLang(lang) {
-  localStorage.setItem("lang", lang);
+    localStorage.setItem("lang", lang);
 }
 
 function applyI18n(lang) {
-  const dict = I18N[lang] || I18N.sk;
+    const dict = I18N[lang] || I18N.sk;
 
-  document.documentElement.lang = lang;
+    document.documentElement.lang = lang;
 
-  document.querySelectorAll("[data-i18n]").forEach(el => {
-    const key = el.getAttribute("data-i18n");
-    if (dict[key]) el.textContent = dict[key];
-  });
+    document.querySelectorAll("[data-i18n]").forEach(el => {
+        const key = el.getAttribute("data-i18n");
+        if (dict[key]) el.textContent = dict[key];
+    });
 
-  // Update dropdown if present
-  const sel = document.querySelector("#langSelect");
-  if (sel) sel.value = lang;
+    // Update dropdown if present
+    const sel = document.querySelector("#langSelect");
+    if (sel) sel.value = lang;
 }
 
 function initI18n() {
-  const lang = getLang();
-  applyI18n(lang);
+    const lang = getLang();
+    applyI18n(lang);
 
-  const sel = document.querySelector("#langSelect");
-  if (sel) {
-    sel.addEventListener("change", (e) => {
-      const newLang = e.target.value;
-      setLang(newLang);
-      applyI18n(newLang);
-    });
-  }
+    const sel = document.querySelector("#langSelect");
+    if (sel) {
+        sel.addEventListener("change", (e) => {
+            const newLang = e.target.value;
+            setLang(newLang);
+            applyI18n(newLang);
+        });
+    }
 }
 
 document.addEventListener("DOMContentLoaded", initI18n);
